58a2eec342e3e353a7f660c2b2e372026588469b
denis <d.karlinsky@andersenlab.com>
2026-05-15 10:47:07 +0300

    test fixed

--- services/gateway/src/test/java/com/agentpay/gateway/GatewayApplicationTest.java
+++ services/gateway/src/test/java/com/agentpay/gateway/GatewayApplicationTest.java
@@ -1,17 +1,16 @@
 package com.agentpay.gateway;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.agentpay.gateway.orchestrator.OrchestratorClient;
 import com.agentpay.gateway.orchestrator.OrchestratorUnavailableException;
